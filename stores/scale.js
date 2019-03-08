57dc56eb1fd53427f86a88dc97cc37e16a83bc5e
Michael Williams <michael.williams@enspiral.com>
2019-03-08 23:16:51 +1300

    properly scale for anything other than C major

--- stores/scale.js
+++ stores/scale.js
@@ -20,14 +20,27 @@ module.exports = function scaleStore (state, emitter) {
   })
 
   function computeNotes () {
-    state.notes = [1, 2, 3, 4, 5, 6].reduce(
-      (sofar, octave) => [
-        ...sofar,
-        ...Tonal.Scale.notes(state.scale.note, state.scale.type).map(note => {
-          return note + octave
-        })
-      ],
-      []
-    )
+    var allNotes = Tonal.Note.names()
+    var scaleNotes = Tonal.Scale.notes(state.scale.note, state.scale.type)
+    var notes = []
+
+    var octave = 1
+    var scaleNoteIndex = 0
+    var previousNote = scaleNotes[0]
+
+    while (octave <= 6) {
+      var note = scaleNotes[scaleNoteIndex]
+
+      if (allNotes.indexOf(previousNote) > allNotes.indexOf(note)) {
+        octave++
+      }
+
+      notes.push(note + octave)
+
+      previousNote = note
+      scaleNoteIndex = (scaleNoteIndex + 1) % scaleNotes.length
+    }
+
+    state.notes = notes
   }
 }
